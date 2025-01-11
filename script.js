6b826bbccc5ef82123cf4ae53a3952ec2442f89b
Vlad-XD <192461579+Vlad-XD@users.noreply.github.com>
2025-01-11 08:52:15 -0800

    Implement the operate function

--- script.js
+++ script.js
@@ -1,3 +1,33 @@
+// variable initializations
+let firstNumber = 0;
+let secondNumber = 0;
+let operator = "";
+
+/*
+  operate: takes two numbers and a string representing a math operation
+           and returns the corresponding resulting number.
+           Note: utilizes same operator string as used in button displays.
+  Parameters: number num1, number num2, string operator
+  Return value: number
+*/
+
+function operate(num1, num2, operator) {
+  switch (operator) {
+    case "+":
+      return add(num1, num2);
+      break;
+    case "-":
+      return subtract(num1, num2);
+      break;
+    case "×":
+      return multiply(num1, num2);
+      break;
+    case "÷":
+      return divide(num1, num2);
+      break;      
+  }
+}
+
 // basic math functions
 
 function add(int1, int2) {
