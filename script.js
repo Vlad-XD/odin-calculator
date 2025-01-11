ced8411e32b7ae3ef52755cfd66d5e2268dc2aab
Vlad-XD <192461579+Vlad-XD@users.noreply.github.com>
2025-01-11 15:06:42 -0800

    Update update CCS for calculator

--- script.js
+++ script.js
@@ -2,13 +2,25 @@
 let firstNumber = 0;
 let secondNumber = 0;
 let operator = "";
+let displayText = "";
+
+/* 
+  updateDisplay: updates the text content of an element acting as our display 
+                  with a passed string.
+  Parameters: Element display, Strind displayText
+  Return value: N/A 
+*/
+
+function updateDisplay(display, displayText) {
+  display.textContent = displayText;
+}
 
 /*
   operate: takes two numbers and a string representing a math operation
            and returns the corresponding resulting number.
            Note: utilizes same operator string as used in button displays.
-  Parameters: number num1, number num2, string operator
-  Return value: number
+  Parameters: Number num1, Number num2, String operator
+  Return value: Number
 */
 
 function operate(num1, num2, operator) {
